d2b64b82fe8f731be955b79916594a6bbef20126
Prayoga <41438462+prayoga5070@users.noreply.github.com>
2024-12-29 07:44:17 +0700

    feat: Admin Movie Store

---
+++ resources/js/Pages/Admin/Movie/Create.js
@@ -0,0 +1,107 @@
+import Authenticated from "@/Layouts/Authenticated/Index";
+import Label from "@/Components/Label";
+import Input from "@/Components/Input";
+import Checkbox from "@/Components/Checkbox";
+import Button from "@/Components/Button";
+import ValidationErrors from "@/Components/ValidationErrors";
+import { Head, useForm } from "@inertiajs/inertia-react";
+
+export default function Create({ auth }) {
+    const { setData, post, processing, errors } = useForm({
+        name: "",
+        category: "",
+        video_url: "",
+        thumbnail: "",
+        rating: "",
+        is_featured: false,
+    });
+
+    const onHandleChange = (event) => {
+        setData(
+            event.target.name,
+            event.target.type === "file"
+                ? event.target.files[0]
+                : event.target.value
+        );
+    };
+
+    const submit = (e) => {
+        e.preventDefault();
+        post(route("admin.dashboard.movie.store"));
+    };
+    return (
+        <Authenticated auth={auth}>
+            <Head title="Admin - Create Movie" />
+            <h1 className="text-xl">Insert a New Movie</h1>
+            <hr className="mb-4"></hr>
+            <ValidationErrors errors={errors} />
+            <form onSubmit={submit}>
+                <Label forInput="name" value="Name" />
+                <Input
+                    type="text"
+                    name="name"
+                    variant="primary-outline"
+                    handleChange={onHandleChange}
+                    placeholder="Enter Name Movie"
+                    isError={errors.name}
+                />
+                <Label forInput="category" value="Category" className="mt-4" />
+                <Input
+                    type="text"
+                    name="category"
+                    variant="primary-outline"
+                    handleChange={onHandleChange}
+                    placeholder="Enter Category Movie"
+                    isError={errors.category}
+                />
+                <Label
+                    forInput="video_url"
+                    value="Video URL"
+                    className="mt-4"
+                />
+                <Input
+                    type="url"
+                    name="video_url"
+                    variant="primary-outline"
+                    handleChange={onHandleChange}
+                    placeholder="Enter Video URL Movie"
+                    isError={errors.video_url}
+                />
+                <Label forInput="thumbnail" value="Thumbnail" className="" />
+                <Input
+                    type="file"
+                    name="thumbnail"
+                    variant="primary-outline"
+                    handleChange={onHandleChange}
+                    placeholder="Insert Thumbnail Movie"
+                    isError={errors.thumbnail}
+                />
+                <Label forInput="rating" value="Rating" className="mt-4" />
+                <Input
+                    type="number"
+                    name="rating"
+                    variant="primary-outline"
+                    handleChange={onHandleChange}
+                    placeholder="Enter Rating Movie"
+                    isError={errors.rating}
+                />
+                <div className="flex flex-row mt-4 items-center">
+                    <Label
+                        forInput="is_featured"
+                        value="Is Featured"
+                        className="mr-3 mt-1"
+                    />
+                    <Checkbox
+                        name="is_featured"
+                        handleChange={(e) =>
+                            setData("is_featured", e.target.checked)
+                        }
+                    />
+                </div>
+                <Button type="submit" className="mt-4" processing={processing}>
+                    Save
+                </Button>
+            </form>
+        </Authenticated>
+    );
+}
